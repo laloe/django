582a5a1380592346dccea336a5367d5a3d89b89a
Eduardo González <laloe.90@gmail.com>
2017-01-12 13:33:53 -0600

    django

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^formulario/$', views.formularioView.as_view(), name='formulario'),
+    url(r'^(?P<pk>[0-9]+)/insercion/$', views.insercionView.as_view(), name='insercion'),
+    url(r'^termino/$', views.termino, name='termino'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -53,3 +53,31 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class formularioView(generic.ListView):
+    template_name = 'polls/formulario.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+    	return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class insercionView(generic.DetailView):
+    model = Question
+    template_name = 'polls/insercion.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+def termino(request):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
